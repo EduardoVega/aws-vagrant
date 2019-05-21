333c642afcc79c26fce42b89c28815af32174b0a
EduardoVega <edvegavalerio@gmail.com>
2019-05-21 08:51:55 -0600

    Remove info from env vars

--- gcp-env-vars.sh
+++ gcp-env-vars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export GCP_PROJECT_ID="development-240318"
-export GCP_SERVICE_ACCOUNT="/Users/edvegavalerio/Documents/Eduardo/google_service_account.json"
-export GCP_IMAGE="cent-test"
-export SSH_USER="edvegavalerio"
-export SSH_PRIVATE_KEY="/Users/edvegavalerio/Documents/Eduardo/google_rsa"
+export GCP_PROJECT_ID=""
+export GCP_SERVICE_ACCOUNT=""
+export GCP_IMAGE=""
+export SSH_USER=""
+export SSH_PRIVATE_KEY=""
